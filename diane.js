08056dc4e8815ac5795c610e22036d6fd47473ab
Maxime Augier <max@xolus.net>
2014-11-16 03:41:15 +0100

    Testing out Socket.IO, basic status interface

--- diane.js
+++ diane.js
@@ -1,5 +1,8 @@
 var app = require('express')()
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var connection_count = 0;
 
 app.get('/', function(req,res) {
     res.sendFile(__dirname + "/status.html")
@@ -7,6 +10,17 @@ app.get('/', function(req,res) {
 
 app.get('/diane.css', function(req,res) {
     res.sendFile(__dirname + "/diane.css");
+});
+
+// Global socket
+io.on('connection', function(socket){
+    console.log("New connection")
+    connection_count++;
+    io.emit('cc', connection_count);
+    socket.on('disconnect', function() {
+        connection_count--;
+        io.emit('cc', connection_count);
+    });
 });
 
 http.listen(3000, function() {
